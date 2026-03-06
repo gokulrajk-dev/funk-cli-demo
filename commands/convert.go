33a3cc90fc63fb8dc9fabe62c3ccb3bbc17a2830
adil <adhilimam92@gmail.com>
2026-03-06 06:01:08 +0530

    fix: fixed a bug in convert.go for proper semantics

--- commands/convert.go
+++ commands/convert.go
@@ -13,11 +13,11 @@ func ConvertCommand() *cli.Command {
 		Action: Converto,
 		Flags: []cli.Flag{
 			&cli.Float64Flag{
-				Name:  "km",
+				Name:  "miles",
 				Usage: "Convert kilometers to Miles",
 			},
 			&cli.Float64Flag{
-				Name:  "miles",
+				Name:  "km",
 				Usage: "Convert miles to kilometers",
 			},
 		},
@@ -25,18 +25,18 @@ func ConvertCommand() *cli.Command {
 }
 
 func Converto(ctx context.Context, cmd *cli.Command) error {
-	if cmd.IsSet("km") {
-		km := cmd.Float64("km")
-		miles := km * 0.6213712
-		fmt.Printf("%.2f km = %.2f miles \n", km, miles)
+	if cmd.IsSet("miles") {
+		km := cmd.Float64("miles")
+		mil := km * 0.6213712
+		fmt.Printf("%.2f km = %.2f miles \n", km, mil)
 		return nil
-	} else if cmd.IsSet("miles") {
-		miles := cmd.Float64("miles")
-		km := miles / 0.6213712
-		fmt.Printf("%.2f miles = %.2f km \n", miles, km)
+	} else if cmd.IsSet("km") {
+		miles := cmd.Float64("km")
+		kim := miles / 0.6213712
+		fmt.Printf("%.2f miles = %.2f km \n", miles, kim)
 		return nil
 	} else {
-		return fmt.Errorf("Please specify either --km or --miles")
+		return fmt.Errorf("use --km or --miles")
 	}
 
 }
