ea0a160f3a79d38ce4851f840a11607b944ea2f1
gokulrajk-dev <kgokulraj.developer@gmail.com>
2026-03-24 17:28:18 +0530

    fix: bug fix

--- go.mod
+++ go.mod
@@ -4,10 +4,18 @@ go 1.25.7
 
 require (
 	github.com/alexeyco/simpletable v1.0.0
+	github.com/fatih/color v1.19.0
 	github.com/mattn/go-sqlite3 v1.14.34
 	github.com/nsf/termbox-go v1.1.1
 	github.com/urfave/cli/v3 v3.7.0
+)
+
+require (
 	github.com/clipperhouse/uax29/v2 v2.7.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.21 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.42.0 // indirect
+	golang.org/x/term v0.41.0
 )
--- sqldb/db.go
+++ sqldb/db.go
@@ -2,16 +2,27 @@ package sqldb
 
 import (
 	"database/sql"
-	"log"
 	"fmt"
-	_ "github.com/mattn/go-sqlite3"
+	"log"
+	"os"
+
 	"github.com/alexeyco/simpletable"
-	
+	"github.com/fatih/color"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+var colorSuccess = color.New(color.FgGreen)
+
 func ConnectDB() *sql.DB {
-	db,err := sql.Open("sqlite3","./funk.db")
-	if err != nil{
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbPath := home + "/.funk.db"
+
+	db, err := sql.Open("sqlite3", dbPath)
+	if err != nil {
 		log.Fatal(err)
 	}
 
@@ -25,7 +36,8 @@ func Create_db() {
 	create := ` CREATE TABLE IF NOT EXISTS Timer (
         id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		create_at DATE DEFAULT (DATE('now')),
-        timer TEXT
+        timer TEXT,
+		name TEXT
     );`
 
 	_,err := db.Exec(create)
@@ -33,32 +45,30 @@ func Create_db() {
 	if err !=nil{
 		log.Fatal(err)
 	}
-
-	log.Println("Table 'users' created successfully")
-
 }
 
-func Insert_data(h int,m int,s int)  {
+func Insert_data(h int,m int,s int,task string) string {
 	db := ConnectDB()
 
 	defer db.Close()
 
-	timer := fmt.Sprintf("%d:%d:%d", h, m, s)
+	timer := fmt.Sprintf("%02d:%02d:%02d", h, m, s)
 
-	_,err :=db.Exec("INSERT INTO Timer(timer) VALUES(?)",timer)
+	_,err :=db.Exec("INSERT INTO Timer(timer,name) VALUES(?,?)",timer,task)
 
 	if err !=nil{
 		log.Fatal(err)
 		
 	}
-	fmt.Printf("\ninsert timer data in db successfully: %02d:%02d:%02d\n Command 'funk timer --his' ", h, m, s)
+
+	return  "\ninsert timer data in db successfully \n Command 'funk timer --his' "
 }
 
 func Show_history()  {
 	db := ConnectDB()
 	defer db.Close()
 
-	row,err := db.Query("SELECT id, DATE(create_at), timer FROM Timer")
+	row,err := db.Query("SELECT id, DATE(create_at), timer, name FROM Timer ORDER BY id DESC")
 
 	if err !=nil{
 		log.Fatal(err)
@@ -73,6 +83,7 @@ func Show_history()  {
 			{Align: simpletable.AlignCenter,Text: "ID"},
 			{Align: simpletable.AlignCenter,Text: "CREATE DATE"},
 			{Align: simpletable.AlignCenter,Text: "TIMER"},
+			{Align: simpletable.AlignCenter,Text: "NAME"},
 		},	
 	}
 
@@ -80,11 +91,13 @@ func Show_history()  {
 		var id int
 		var created string
 		var timer string
-		row.Scan(&id,&created,&timer)
+		var name string
+		row.Scan(&id,&created,&timer,&name)
 		r := []*simpletable.Cell{
 			{Align: simpletable.AlignRight, Text: fmt.Sprintf("%d", id)},
 			{Text: created},
 			{Align: simpletable.AlignCenter, Text: timer},
+			{Align: simpletable.AlignCenter, Text: name},
 		}
 		table.Body.Cells = append(table.Body.Cells,r)
 	}
@@ -113,5 +126,31 @@ func  Delete_Record(index int)  {
 		return
 	}
 
-	fmt.Println("record delete successfully")
+	colorSuccess = color.New(color.FgRed)
+
+	colorSuccess.Println("record delete successfully")
+}
+
+func  Delete_All_Record()  {
+	db := ConnectDB()
+	defer db.Close()
+
+	record,err:=db.Exec("DELETE FROM Timer")
+
+	if err !=nil{
+		log.Fatal(err)
+	}
+	row,err  := record.RowsAffected()
+
+	if err !=nil{
+		log.Fatal(err)
+	}
+
+	if row ==0 {
+		fmt.Println("no record found or record already delete")
+		return
+	}
+
+	colorSuccess = color.New(color.FgRed)
+	colorSuccess.Println("All Record delete successfully")
 }
